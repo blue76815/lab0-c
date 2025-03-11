e8449fec6aa6a7ee7cd459a00973e23a7141688b
blue76815 <blue76815@gmail.com>
2025-03-11 13:53:11 +0800

    Add q_reverse function
    The function traverses the list and swaps each node
    
    Change-Id: I0f625a458a7f1454fff2bb58f62699ac22aa1536

--- queue.c
+++ queue.c
@@ -182,7 +182,24 @@ void q_swap(struct list_head *head)
 }
 
 /* Reverse elements in queue */
-void q_reverse(struct list_head *head) {}
+void q_reverse(struct list_head *head)
+{
+    if (head == NULL || list_empty(head)) {
+        return;
+    }
+    struct list_head *node = head->next, *next_node, *tmp_node;
+    while (node != head) {
+        next_node = node->next;
+        tmp_node = node->next;
+        node->next = node->prev;
+        node->prev = tmp_node;
+        node = next_node;
+    }
+    tmp_node = head->next;
+    head->next = head->prev;
+    head->prev = tmp_node;
+}
+
 
 /* Reverse the nodes of the list k at a time */
 void q_reverseK(struct list_head *head, int k)
