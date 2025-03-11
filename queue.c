53c2ae472257a9fd1d34c8fcfa803edf46fbbdf5
blue76815 <blue76815@gmail.com>
2025-03-11 13:27:28 +0800

    Add q_delete_mid function
    We utilize Fast-slow Pointers approach to reduce the time complex.
    
    Change-Id: I7d5245160090ebb89de6aa4cfe25e5fa80352e55

--- queue.c
+++ queue.c
@@ -130,6 +130,20 @@ int q_size(struct list_head *head)
 bool q_delete_mid(struct list_head *head)
 {
     // https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
+    if (head == NULL || list_empty(head))
+        return false;
+
+    struct list_head *fast = head->next;
+    struct list_head *slow = head->next;
+    while (fast != head && fast->next != head) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    element_t *ele_node = list_entry(slow, element_t, list);
+    list_del(slow);
+    free(ele_node->value);
+    free(ele_node);
     return true;
 }
 
