f1efcf002655e8bedb1cae36823f91410eedc3c7
blue76815 <blue76815@gmail.com>
2025-03-11 15:25:49 +0800

    Add q_swap function
    We use list_swap,list_replace from list.h to deal with swap node.
    
    Change-Id: I254582fa70fe652a9e4dc8c58e1a032773c6d2ae

--- queue.c
+++ queue.c
@@ -175,6 +175,40 @@ bool q_delete_dup(struct list_head *head)
     return true;
 }
 
+/**
+ * https://github.com/torvalds/linux/blob/master/include/linux/list.h
+ * list_replace - replace old entry by new one
+ * @old : the element to be replaced
+ * @new : the new element to insert
+ *
+ * If @old was empty, it will be overwritten.
+ */
+static inline void list_replace(struct list_head *old, struct list_head *new)
+{
+    new->next = old->next;
+    new->next->prev = new;
+    new->prev = old->prev;
+    new->prev->next = new;
+}
+
+/**
+ * https://github.com/torvalds/linux/blob/master/include/linux/list.h
+ * list_swap - replace entry1 with entry2 and re-add entry1 at entry2's position
+ * @entry1: the location to place entry2
+ * @entry2: the location to place entry1
+ */
+static inline void list_swap(struct list_head *entry1, struct list_head *entry2)
+{
+    struct list_head *pos = entry2->prev;
+
+    list_del(entry2);
+    list_replace(entry1, entry2);
+    // list_splice(entry1, entry2);
+    if (pos == entry1)
+        pos = entry2;
+    list_add(entry1, pos);
+}
+
 /* Swap every two adjacent nodes */
 void q_swap(struct list_head *head)
 {
