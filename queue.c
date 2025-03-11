1c580065612322823fad6fb05d4cc5f0c5ce4887
blue76815 <blue76815@gmail.com>
2025-03-11 13:11:58 +0800

    Implement q_remove_tail functions
    The function is used to remove an element at tail of queue.
    
    Change-Id: I5cdfa6e095343495d60b6d1ac2148c86ee00c213

--- queue.c
+++ queue.c
@@ -94,7 +94,21 @@ element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (head == NULL || list_empty(head))
+        return NULL;
+
+    element_t *node = list_last_entry(head, element_t, list);
+
+    list_del_init(&(node->list));
+
+    if (sp) {
+        size_t length = strlen(node->value) + 1;
+        length = length > bufsize ? bufsize : length;
+        memset(sp, '\0', length);
+        memcpy(sp, node->value, length - 1);
+    }
+
+    return node;
 }
 
 /* Return number of elements in queue */
